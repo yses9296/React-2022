7dd61520f22ba1736a5e8d52fc2b228efea9c1b0
Eunseo Choi <yses9296@gmail.com>
2023-01-09 00:18:54 +0900

    React-redux

--- react-redux/src/UseRedux.js
+++ react-redux/src/UseRedux.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 import './App.css';
-import { createStore } from 'redux'
-const store = createStore();
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+function reducer(currentState, action){
+    // init
+    if(currentState === undefined){
+        return {
+            num: 1
+        }
+    }
+    
+    const newState = {...currentState} //현재 값 복제해서 새로운 변수 생성
+
+    if(action.type === 'ADD'){
+        newState.num++;
+    }
+    else if(action.type === 'SUBTRACT'){
+        newState.num--;
+    }
+    return newState
+}
+const _store = createStore(reducer);
+
 const UseRedux = () => {
     
-    const [num, setNum] = useState(1);
-    const onClickHandler = () => {
-        setNum(num + 1)
-    }
+    // const [num, setNum] = useState(1);
     return (
         <div id="container">
-        <h1>Root : {num} </h1>
+        <h1>Root :  </h1>
         <div id='grid'>
-            <Left1/>
-            <Right1/>
+            <Provider store={_store}>
+                <Left1/>
+                <Right1/>
+            </Provider>
         </div>
         </div>
     );
@@ -37,9 +56,13 @@ const Left2 = (props) => {
     )
 }
 const Left3 = (props) => {
+    // function getNum(state){
+    //     return state.num
+    // }
+    const number = useSelector(state => state.num);
     return(
         <div>
-        <h1>Left3 : </h1>
+        <h1>Left3 : {number}</h1>
         </div>
     )
 }
@@ -61,10 +84,12 @@ const Right2 = (props) => {
     )
 }
 const Right3 = (props) => {
+    const dispatch = useDispatch();
     return(
         <div>
-        <h1>Right3 : </h1>
-        <button onClick={() => {}}>Add</button>
+            <h1>Right3 : </h1>
+            <button onClick={() => { dispatch({ type:'ADD' }) }}>Add</button>
+            <button onClick={() => { dispatch({ type:'SUBTRACT' }) }}>SUBTRACT</button>
         </div>
     )
 }
